1fcc0fb569f1c8a578d757644db0d01b2e7a6d95
piash071 <piashcse.ph@gmail.com>
2024-03-11 19:37:48 +0600

    add some data and make grid

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,15 @@
 
 import './Country.css'
 const Country = ({country}) => {
-    const {name, flags} = country;
+    const {name, flags, capital, population, area, cca3} = country;
     return (
         <div className="country">
             <h3>Name: {name.common}</h3>
             <img src={flags.png} alt="" />
+            <p>Capital: {capital}</p>
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <p><small>Code: {cca3}</small></p>
         </div>
     );
 };
